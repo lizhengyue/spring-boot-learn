2971a336fdb1905e908624978f1bb88c84e9ba76
lizhengyue <290558053@qq.com>
2021-01-24 17:38:15 +0800

    测试git

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -18,7 +18,7 @@ public class UserController {
    // @ResponseBody
     public JSONResult hello(){
         User u = new User();
-        u.setName("7891");
+        u.setName("789178");
         return JSONResult.ok(u);
     }
 
